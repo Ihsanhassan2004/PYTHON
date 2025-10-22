6d05c34b9db9c88ba1820fa70820937641bf53e1
Ihsanhassan2004 <ihsanhasaan22@gmail.com>
2025-10-22 15:48:00 +0530

    completed

---
+++ record 14/newarea.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+def arr(l,b):
+    print("area of rectancle is:",l*b)
+def arc(r):
+    print("area of circle is",3.14*r*r)
+def prr(l,b):
+     print("perimeter of rectancle:",2*(l+b))
+def prc(r):
+     print("perimeter of circle:",2*3.14*r)
+
+
+# In[ ]:
+
+
+
+
